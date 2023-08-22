ff3be8ab182f84c25b99be221ee4994a428c8d4d
shakil <shakilkhanhu@gmail.com>
2023-08-22 14:50:56 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/edulogy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.edulogy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/BottomNav/bottom_nav.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class BottomNavigationScreen extends StatefulWidget {
+  const BottomNavigationScreen({super.key});
+
+  @override
+  State<BottomNavigationScreen> createState() => _BottomNavigationScreenState();
+}
+
+class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
