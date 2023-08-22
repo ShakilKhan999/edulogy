e06c988a421f1ab8d09740c776a9edeaf78b6c54
SyedMahiuddin <syedmahiuddin01@gmail.com>
2023-08-22 17:07:36 +0600

    config bottomnav

---
+++ lib/mock_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MockPage extends StatelessWidget {
+  const MockPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("MockPage"),);
+  }
+}
