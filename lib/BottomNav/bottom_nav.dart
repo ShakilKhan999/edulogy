b984850a668ea0de05530835c9926ae5d653fb8d
shakil <shakilkhanhu@gmail.com>
2023-08-22 16:29:49 +0600

    bottom_nav.dart

--- lib/BottomNav/bottom_nav.dart
+++ lib/BottomNav/bottom_nav.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:edulogy/const/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BottomNavigationScreen extends StatefulWidget {
   const BottomNavigationScreen({super.key});
@@ -8,8 +11,30 @@ class BottomNavigationScreen extends StatefulWidget {
 }
 
 class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
+  int _currentIndex = 2;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+      bottomNavigationBar: CurvedNavigationBar(
+        index: _currentIndex,
+        backgroundColor: Colors.transparent,
+        color:  AppColors.blue,
+        animationDuration: Duration(milliseconds: 300),
+        items: [
+          ImageIcon(const AssetImage('assets/images/course.png',),size: 30,color: AppColors.backgroundcolor,),
+          ImageIcon(const AssetImage('assets/images/doubt.png'),size: 30,color: AppColors.backgroundcolor),
+          Icon(Icons.home,size: 40,color: AppColors.backgroundcolor,),
+          ImageIcon(const AssetImage('assets/images/exam.png'),size: 30,color: AppColors.backgroundcolor),
+          ImageIcon(const AssetImage('assets/images/quiz.png'),size: 30,color: AppColors.backgroundcolor),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
